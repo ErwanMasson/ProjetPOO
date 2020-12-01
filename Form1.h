59843c67fafca5bb2d55f2ca028e28742ef486cf
ErwanMasson <75259826+ErwanMasson@users.noreply.github.com>
2020-12-01 14:59:32 +0100

    Update Form1.h

--- Form1.h
+++ Form1.h
@@ -15,9 +15,6 @@ namespace CppCLRWinformsProjekt {
 		Form1(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Konstruktorcode hier hinzufügen.
-			//
 		}
 
 	protected:
@@ -28,6 +25,13 @@ namespace CppCLRWinformsProjekt {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	protected:
 
 	private:
 		System::ComponentModel::Container ^components;
@@ -35,12 +39,91 @@ namespace CppCLRWinformsProjekt {
 #pragma region Windows Form Designer generated code
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14));
+			this->label1->Location = System::Drawing::Point(82, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(74, 24);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Gestion";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->button1->Location = System::Drawing::Point(12, 55);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(98, 29);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Personnel";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->button2->Location = System::Drawing::Point(131, 55);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(98, 29);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"Clients";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->button3->Location = System::Drawing::Point(12, 105);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(98, 29);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"Stock";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->button4->Location = System::Drawing::Point(131, 105);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(98, 29);
+			this->button4->TabIndex = 4;
+			this->button4->Text = L"Commandes";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
+			this->button5->Location = System::Drawing::Point(70, 153);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(98, 29);
+			this->button5->TabIndex = 5;
+			this->button5->Text = L"Statistiques";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(241, 194);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+};
 }
